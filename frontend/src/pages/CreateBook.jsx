fece3c92e49013002c966d13f4ba61adfc66cbf6
Draxsis <Draxsis.1995@gmail.com>
2023-10-04 12:05:54 +0330

    Updated

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
-import { Box, Button, TextField, Typography, Container} from "@mui/material";
+import { Box, Button, TextField, Typography, Container } from "@mui/material";
 
 const CreateBooks = () => {
   const [title, setTitle] = useState("");
@@ -36,51 +36,53 @@ const CreateBooks = () => {
   };
 
   return (
-    <Container maxWidth="sm" style={{ paddingTop: "20px", marginTop: "100px" }}>
-      <BackButton />
-      <Typography
-        variant="h4"
-        style={{ marginBottom: "20px", marginTop: "30px" }}
-      >
-        Create Book
-      </Typography>
-      {loading && <Spinner />}
-      <Box>
-        <TextField
-          label="Title"
-          variant="outlined"
-          fullWidth
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          label="Author"
-          variant="outlined"
-          fullWidth
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          label="Publish Year"
-          variant="outlined"
-          fullWidth
-          type="number"
-          value={publishYear}
-          onChange={(e) => setPublishYear(e.target.value)}
-          style={{ marginBottom: "20px" }}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={handleSaveBook}
-          disabled={loading}
-        >
-          Save
-        </Button>
-      </Box>
+    <Container maxWidth="sm" style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#f0f0f0", padding: "20px", marginTop: '9%' }}>
+      <div style={{ flex: 1, marginRight: "20px" }}>
+        <BackButton />
+        <Typography variant="h4" style={{ marginLeft: "15px", marginBottom: "20px", marginTop: "30px" }}>
+          Create Book
+        </Typography>
+        {loading && <Spinner />}
+        <Box>
+          <TextField
+            label="Title"
+            variant="outlined"
+            fullWidth
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            label="Author"
+            variant="outlined"
+            fullWidth
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            label="Publish Year"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            style={{ marginBottom: "20px" }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleSaveBook}
+            disabled={loading}
+          >
+            Save
+          </Button>
+        </Box>
+      </div>
+      <div style={{ flex: 1, backgroundColor: "#ffffff", padding: "20px" }}>
+        {/* Content for the right side (if any) */}
+      </div>
     </Container>
   );
 };
