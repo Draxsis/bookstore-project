fe23eadc59a058814762b684b65ad1e5a7b30b01
mohammad mahdi <mohammad mahdi>
2023-10-07 21:11:28 +0330

    ShowBook update

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,29 +1,65 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import TitleIcon from '@mui/icons-material/Title';
+import PersonIcon from '@mui/icons-material/Person';
+import EventIcon from '@mui/icons-material/Event';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const Container = styled(Box)({
-  padding: '1rem',
-  border: '2px solid #90caf9',
-  borderRadius: '12px',
+  display: 'flex',
+  height: '100vh', // 100% of the viewport height
+});
+
+const LeftSection = styled(Box)({
+  flex: 1,
+  backgroundColor: '#e0e0e0',
+  display: 'flex',
+  flexDirection: 'column', // Adjusted to column layout
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '#fff',
+});
+
+const RightSection = styled(Box)({
+  flex: 1, // Adjusted to 1 to make it 50% of the screen
+  padding: '2rem',
+});
+
+const DetailBox = styled(Box)({
+  marginBottom: '1.5rem',
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const Icon = styled(Box)({
+  marginRight: '0.5rem',
+  color: '#2C3639',
 });
 
 const Label = styled(Typography)({
   fontSize: '1rem',
-  marginRight: '0.5rem',
-  color: 'rgba(0, 0, 0, 0.7)',
   fontWeight: 'bold',
+  color: '#2C3639',
 });
 
 const Value = styled(Typography)({
   fontSize: '1rem',
-  color: 'rgba(0, 0, 0, 0.9)',
+  color: '#263238',
+});
+
+const Heading = styled(Typography)({
+  fontSize: '2rem',
+  color: '#263238',
+  fontWeight: 'bold',
+  marginBottom: '1.5rem',
 });
 
 const ShowBook = () => {
@@ -46,41 +82,64 @@ const ShowBook = () => {
   }, [id]);
 
   return (
-    <Box p={4}>
-      <BackButton />
-      <Typography variant="h4" gutterBottom>
-        Show Book
-      </Typography>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Paper component={Container}>
-          <div className="my-4">
-            <Label component="span">Id:</Label>
-            <Value>{book._id}</Value>
-          </div>
-          <div className="my-4">
-            <Label component="span">Title:</Label>
-            <Value>{book.title}</Value>
-          </div>
-          <div className="my-4">
-            <Label component="span">Author:</Label>
-            <Value>{book.author}</Value>
-          </div>
-          <div className="my-4">
-            <Label component="span">Publish Year:</Label>
-            <Value>{book.publishYear}</Value>
-          </div>
-          <div className="my-4">
-            <Label component="span">Create Time:</Label>
-            <Value>{new Date(book.createdAt).toLocaleString()}</Value>
-          </div>
-          <div className="my-4">
-            <Label component="span">Last Update Time:</Label>
-            <Value>{new Date(book.updatedAt).toLocaleString()}</Value>
-          </div>
-        </Paper>
-      )}
+    <Box>
+      <Paper component={Container}>
+        <LeftSection>
+          {/* Left section content (if any) */}
+        </LeftSection>
+        <RightSection>
+          <Heading>Book Details</Heading>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              <DetailBox>
+                <Icon>
+                  <AssignmentIndIcon />
+                </Icon>
+                <Label>ID:</Label>
+                <Value>{book._id}</Value>
+              </DetailBox>
+              <DetailBox>
+                <Icon>
+                  <TitleIcon />
+                </Icon>
+                <Label>Title:</Label>
+                <Value>{book.title}</Value>
+              </DetailBox>
+              <DetailBox>
+                <Icon>
+                  <PersonIcon />
+                </Icon>
+                <Label>Author:</Label>
+                <Value>{book.author}</Value>
+              </DetailBox>
+              <DetailBox>
+                <Icon>
+                  <EventIcon />
+                </Icon>
+                <Label>Publish Year:</Label>
+                <Value>{book.publishYear}</Value>
+              </DetailBox>
+              <DetailBox>
+                <Icon>
+                  <AccessTimeIcon />
+                </Icon>
+                <Label>Created At:</Label>
+                <Value>{new Date(book.createdAt).toLocaleString()}</Value>
+              </DetailBox>
+              <DetailBox>
+                <Icon>
+                  <AccessTimeIcon />
+                </Icon>
+                <Label>Last Updated At:</Label>
+                <Value>{new Date(book.updatedAt).toLocaleString()}</Value>
+              </DetailBox>
+              <BackButton />
+            </>
+          )}
+        </RightSection>
+      </Paper>
     </Box>
   );
 };
