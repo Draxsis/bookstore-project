a33775a76ca196b9010cd88148a56bb658410cfa
mohammad mahdi <mohammad mahdi>
2023-10-07 19:50:41 +0330

    updated footer to origin

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,10 +2,10 @@ import { Container, Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <footer style={{ marginTop: '4.1rem', padding: '1rem 0', backgroundColor: 'rgb(44, 54, 57)', textAlign: 'center' }}>
+    <footer style={{ marginTop: '2rem', marginBottom: '2rem' }}>
       <Container maxWidth="md">
-        <Typography variant="body2" style={{ color: 'white', lineHeight: '2rem' }}>
-          Made by <a href="http://www.github.com/Draxsis" target="_blank" rel="noopener noreferrer" style={{ color: "lightblue" }}>Draxsis</a> with love ❤️.
+        <Typography variant="body2" color="textSecondary" align="center">
+          Made by <a href="http://www.github.com/Draxsis" target="_blank" rel="noopener noreferrer" style={{color:"black"}}>Draxsis</a> with love ❤️.
         </Typography>
       </Container>
     </footer>
