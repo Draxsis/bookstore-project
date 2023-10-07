50f6144c4668b6570b4154a3c0b2acf0ab189653
Draxsis <Draxsis.1995@gmail.com>
2023-10-07 21:11:39 +0330

    update the edit,delete and create pages

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
-import { Box, Button, TextField, Typography, Container } from "@mui/material";
+import { TextField, Button, Box, Typography, Container } from "@mui/material";
+import "./CreateBook.css";
 
 const CreateBooks = () => {
   const [title, setTitle] = useState("");
@@ -36,54 +37,104 @@ const CreateBooks = () => {
   };
 
   return (
-    <Container maxWidth="sm" style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#f0f0f0", padding: "20px", marginTop: '9%' }}>
-      <div style={{ flex: 1, marginRight: "20px" }}>
-        <BackButton />
-        <Typography variant="h4" style={{ marginLeft: "15px", marginBottom: "20px", marginTop: "30px" }}>
-          Create Book
-        </Typography>
-        {loading && <Spinner />}
-        <Box>
-          <TextField
-            label="Title"
-            variant="outlined"
-            fullWidth
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            style={{ marginBottom: "10px" }}
-          />
-          <TextField
-            label="Author"
-            variant="outlined"
-            fullWidth
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            style={{ marginBottom: "10px" }}
-          />
-          <TextField
-            label="Publish Year"
-            variant="outlined"
-            fullWidth
-            type="number"
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
-            style={{ marginBottom: "20px" }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={handleSaveBook}
-            disabled={loading}
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div
+        style={{
+          flex: 1,
+          backgroundColor: "#e0e0e0",
+          padding: "20px",
+          borderRadius: "0",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      ></div>
+      <div
+        style={{
+          flex: 1,
+          padding: "20px",
+          backgroundColor: "#ffffff",
+          borderRadius: "0 16px 16px 0",
+        }}
+      >
+        <Container
+          maxWidth="sm"
+          style={{ paddingTop: "20px", textAlign: "center", height: "100%" }}
+        >
+          <BackButton />
+          <Typography
+            variant="h4"
+            style={{
+              marginBottom: "20px",
+              marginTop: "20px",
+              overflow: "hidden",
+              borderRight: "0.15em solid orange",
+              whiteSpace: "nowrap",
+              letterSpacing: "0.15em",
+              animation:
+                "typing 1.5s steps(10, end), blink-caret 0.75s step-end infinite",
+            }}
           >
-            Save
-          </Button>
-        </Box>
+            Create
+          </Typography>
+          {loading && <Spinner />}
+          <Box
+            sx={{
+              border: "2px solid #616161",
+              borderRadius: "16px",
+              padding: "20px",
+              width: "80%",
+              margin: "0 auto",
+            }}
+          >
+            <div style={{ marginBottom: "20px" }}>
+              <Typography variant="h6" style={{ color: "#616161" }}>
+                Title
+              </Typography>
+              <TextField
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+              <Typography variant="h6" style={{ color: "#616161" }}>
+                Author
+              </Typography>
+              <TextField
+                type="text"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+              <Typography variant="h6" style={{ color: "#616161" }}>
+                Publish Year
+              </Typography>
+              <TextField
+                type="number"
+                value={publishYear}
+                onChange={(e) => setPublishYear(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSaveBook}
+              style={{ borderRadius: "8px", padding: "10px 20px" }}
+            >
+              Create
+            </Button>
+          </Box>
+        </Container>
       </div>
-      <div style={{ flex: 1, backgroundColor: "#ffffff", padding: "20px" }}>
-        {/* Content for the right side (if any) */}
-      </div>
-    </Container>
+    </div>
   );
 };
 
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -54,62 +54,65 @@ const EditBook = () => {
   };
 
   return (
-    <Container maxWidth="sm" style={{ paddingTop: '20px', textAlign: 'center', marginTop: '50px' }}>
-      <BackButton />
-      <Typography variant="h4" style={{ marginBottom: '20px', marginTop: '30px' }}>
-        Edit Book
-      </Typography>
-      {loading && <Spinner />}
-      <Box
-        sx={{
-          border: '2px solid #616161',
-          borderRadius: '16px',
-          padding: '20px',
-          width: '80%',
-          margin: '0 auto',
-        }}
-      >
-        <div style={{ marginBottom: '20px' }}>
-          <Typography variant="h6" style={{ marginRight: '20px', color: '#616161' }}>
-            Title
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, backgroundColor: '#e0e0e0', padding: '20px', borderRadius: '16px 0 0 16px' }}>
+        <Typography variant="h4" style={{ marginBottom: '20px' }}>
+          Left Section
+        </Typography>
+        <p>Add your content for the left section here.</p>
+      </div>
+      <div style={{ flex: 1, padding: '20px', backgroundColor: '#ffffff', borderRadius: '0 16px 16px 0' }}>
+        <Container maxWidth="sm" style={{ paddingTop: '20px', textAlign: 'center', height: '100%' }}>
+          <BackButton />
+          <Typography variant="h4" style={{ marginBottom: '20px', marginTop: '30px' }}>
+            Edit Book
           </Typography>
-          <TextField
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </div>
-        <div style={{ marginBottom: '20px' }}>
-          <Typography variant="h6" style={{ marginRight: '20px', color: '#616161' }}>
-            Author
-          </Typography>
-          <TextField
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </div>
-        <div style={{ marginBottom: '20px' }}>
-          <Typography variant="h6" style={{ marginRight: '20px', color: '#616161' }}>
-            Publish Year
-          </Typography>
-          <TextField
-            type="number"
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </div>
-        <Button variant="contained" color="primary" onClick={handleEditBook}>
-          Save
-        </Button>
-      </Box>
-    </Container>
+          {loading && <Spinner />}
+          <Box
+            sx={{
+              border: '2px solid #616161',
+              borderRadius: '16px',
+              padding: '20px',
+              width: '80%',
+              margin: '0 auto',
+            }}
+          >
+            <div style={{ marginBottom: '20px' }}>
+              <Typography variant="h6" style={{ color: '#616161' }}>
+                Title
+              </Typography>
+              <TextField type="text" value={title} onChange={(e) => setTitle(e.target.value)} variant="outlined" fullWidth />
+            </div>
+            <div style={{ marginBottom: '20px' }}>
+              <Typography variant="h6" style={{ color: '#616161' }}>
+                Author
+              </Typography>
+              <TextField type="text" value={author} onChange={(e) => setAuthor(e.target.value)} variant="outlined" fullWidth />
+            </div>
+            <div style={{ marginBottom: '20px' }}>
+              <Typography variant="h6" style={{ color: '#616161' }}>
+                Publish Year
+              </Typography>
+              <TextField
+                type="number"
+                value={publishYear}
+                onChange={(e) => setPublishYear(e.target.value)}
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleEditBook}
+              style={{ borderRadius: '8px', padding: '10px 20px' }}
+            >
+              Save
+            </Button>
+          </Box>
+        </Container>
+      </div>
+    </div>
   );
 };
 
